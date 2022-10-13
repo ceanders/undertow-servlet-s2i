ca28d7a0f0fd28505c8f89f86a6d4b33e27bc46f
Cedric Anderson <ceanders@redhat.com>
2022-10-13 11:44:00 -0400

    switched to new project

--- src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello OpenShift - This is Cedric after a trigger")
+                                    .addInitParam("message", "Hello OpenShift - This is Cedric after a trigger - new project!")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
