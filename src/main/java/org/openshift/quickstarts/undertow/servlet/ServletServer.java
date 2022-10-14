aaec6b4c8bb3b82c693069df8654fe35d3742264
Cedric Anderson <73545098+ceanders@users.noreply.github.com>
2022-10-14 11:33:59 -0400

    Update ServletServer.java

--- src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello OpenShift! TAP")
+                                    .addInitParam("message", "Hello OpenShift! TAP2")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
