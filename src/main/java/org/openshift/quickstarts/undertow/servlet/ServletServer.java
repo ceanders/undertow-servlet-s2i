907cd9561a46e5f9114f9b416b9254a2b30b6e35
Cedric Anderson <ceanders@redhat.com>
2022-10-21 15:26:15 -0400

    welcome page for PROD

--- src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello folks, first deployment. Exciting!")
+                                    .addInitParam("message", "Hello folks, this is the MAIN branch we use it for production!")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
