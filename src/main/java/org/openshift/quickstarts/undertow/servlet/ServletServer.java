ab2a4deee84cf70a365cd55228c424eccc87961d
Cedric Anderson <73545098+ceanders@users.noreply.github.com>
2022-10-25 13:09:53 -0400

    updated greeting

--- src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello SAs. Exciting times! Deploy failed so lets use a Trigger.")
+                                    .addInitParam("message", "Welcome to my first app build on OpenShift!")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
